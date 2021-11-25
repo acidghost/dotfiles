d0d7ec0aa9ecad9523156be22e7b3b9f839ee7ea
Andrea Jemmett <1787979+acidghost@users.noreply.github.com>
2021-11-25 16:05:24 +0100

    [shell] fix use of show-off

--- shell/functions.sh
+++ shell/functions.sh
@@ -5,7 +5,7 @@ show-off() {
 }
 
 vusec-show-off() {
-    show_off --source ~/vusec.ascii --ascii_colors 0 1 2 3 4 5 6 7 8
+    show-off --source ~/vusec.ascii --ascii_colors 0 1 2 3 4 5 6 7 8
 }
 
 md-read-as-html() {
