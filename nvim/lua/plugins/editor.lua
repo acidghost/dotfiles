f92eb2554586879d054c969d0c0f602441ceee1b
Andrea Jemmett <1787979+acidghost@users.noreply.github.com>
2025-01-20 20:18:56 +0100

    [nvim] lazy-load neo-tree when opening a directory

--- nvim/lua/plugins/editor.lua
+++ nvim/lua/plugins/editor.lua
@@ -31,6 +31,31 @@ return {
         desc = "Buffer Explorer",
       },
     },
+    init = function()
+      -- load neo-tree on BufEnter, if we're opening a directory
+      vim.api.nvim_create_autocmd("BufEnter", {
+        -- make a group to be able to delete it later
+        group = vim.api.nvim_create_augroup("NeoTreeInit", { clear = true }),
+        callback = function()
+          local f = vim.fn.expand("%:p")
+          if vim.fn.isdirectory(f) ~= 0 then
+            vim.cmd("Neotree current dir=" .. f)
+            -- neo-tree is loaded now, delete the init autocmd
+            vim.api.nvim_clear_autocmds({ group = "NeoTreeInit" })
+          end
+        end,
+      })
+    end,
+    opts = {
+      sources = { "filesystem", "buffers", "git_status" },
+      open_files_do_not_replace_types = { "terminal", "Trouble", "trouble", "qf", "Outline" },
+      filesystem = {
+        bind_to_cwd = false,
+        follow_current_file = { enabled = true },
+        use_libuv_file_watcher = true,
+        hijack_netrw_behavior = "open_default",
+      },
+    },
   },
 
   {
