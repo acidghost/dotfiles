774946003f2f56a86da212d2559572bbd572253e
Andrea Jemmett <1787979+acidghost@users.noreply.github.com>
2025-01-21 19:16:34 +0100

    [nvim] lazy-load copilot.vim

--- nvim/lua/plugins/copilot.lua
+++ nvim/lua/plugins/copilot.lua
@@ -1,24 +1,25 @@
 return {
   {
     "github/copilot.vim",
+    keys = {
+      { "<Leader>lce", "<cmd>Copilot enable<cr>",     mode = "n", desc = "Copilot enable" },
+      { "<Leader>lcd", "<cmd>Copilot disable<cr>",    mode = "n", desc = "Copilot disable" },
+      { "<Leader>lcs", "<cmd>Copilot status<cr>",     mode = "n", desc = "Copilot status" },
+      { "<Leader>lcp", "<cmd>Copilot panel<cr>",      mode = "n", desc = "Copilot panel" },
+      { "<C-]>",       "<Plug>(copilot-dismiss)",     mode = "i", desc = "Copilot dismiss" },
+      { "<M-Bslash>",  "<Plug>(copilot-suggest)",     mode = "i", desc = "Copilot suggest" },
+      { "<M-C-Right>", "<Plug>(copilot-accept-line)", mode = "i", desc = "Copilot accept line" },
+      { "<M-Right>",   "<Plug>(copilot-accept-word)", mode = "i", desc = "Copilot accept word" },
+      { "<M-[>",       "<Plug>(copilot-previous)",    mode = "i", desc = "Copilot previous" },
+      { "<M-]>",       "<Plug>(copilot-next)",        mode = "i", desc = "Copilot next" },
+    },
     init = function()
+      -- disable copilot for all filetypes
+      -- enable it for specific filetypes on-demand
+      -- see also nvim-copilot in shell/functions.sh
       vim.g.copilot_filetypes = {
         ["*"] = false,
       }
-
-      vim.cmd([[
-        nmap <Leader>lce :Copilot enable<CR>
-        nmap <Leader>lcd :Copilot disable<CR>
-        nmap <Leader>lcs :Copilot status<CR>
-        nmap <Leader>lcp :Copilot panel<CR>
-
-        imap <C-]>         <Plug>(copilot-dismiss)
-        imap <M-C-Right>   <Plug>(copilot-accept-line)
-        imap <M-Right>     <Plug>(copilot-accept-word)
-        imap <M-Bslash>    <Plug>(copilot-suggest)
-        imap <M-[>         <Plug>(copilot-previous)
-        imap <M-]>         <Plug>(copilot-next)
-      ]])
     end,
   },
 }
