5e28cfcb0f82ba427ee05acf9a3027d6d49e567b
acidghost <1787979+acidghost@users.noreply.github.com>
2025-01-22 10:35:15 +0100

    [nvim] fix telescope open_cwd_dir

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -6,11 +6,12 @@ for _, cmd in ipairs({ "make", "cmake", "gmake" }) do
   end
 end
 
-local pick = function(builtin, opts)
+local function pick(builtin, opts)
   return function()
     opts = opts or {}
     opts.follow = opts.follow ~= false
     if opts.cwd and opts.cwd ~= vim.uv.cwd() then
+      -- TODO: what's the point of this?
       local function open_cwd_dir()
         local action_state = require("telescope.actions.state")
         local line = action_state.get_current_line()
@@ -20,12 +21,12 @@ local pick = function(builtin, opts)
             root = false,
             default_text = line,
           })
-        )
+        )()
       end
       ---@diagnostic disable-next-line: inject-field
       opts.attach_mappings = function(_, map)
         -- opts.desc is overridden by telescope, until it's changed there is this fix
-        map("i", "<a-c>", open_cwd_dir, { desc = "Open cwd Directory" })
+        map("n", "<C-\\>", open_cwd_dir, { desc = "Open cwd Directory" })
         return true
       end
     end
