6a86149ea39cdee6e7a2c8cfeb0e03178b85924f
Andrea Jemmett <1787979+acidghost@users.noreply.github.com>
2025-01-24 20:41:18 +0100

    [nvim] lazy.nvim install colorscheme and disable change_detection

--- nvim/lua/config/lazy.lua
+++ nvim/lua/config/lazy.lua
@@ -38,5 +38,6 @@ require("lazy").setup({
   },
   -- Configure any other settings here. See the documentation for more details.
   -- colorscheme that will be used when installing plugins.
-  install = { colorscheme = { "habamax" } },
+  install = { colorscheme = { "sorbet" } },
+  change_detection = { enabled = false },
 })
